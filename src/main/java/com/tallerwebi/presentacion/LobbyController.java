927be66d47dd1b2d30168c4fe996ef514119465c
Juan Pablo <valenrod2005@gmail.com>
2026-09-17 12:50:49 -0300

    Agrega lobby y resumen

---
+++ src/main/java/com/tallerwebi/presentacion/LobbyController.java
@@ -0,0 +1,17 @@
+package com.tallerwebi.presentacion;
+
+import java.util.Map;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class LobbyController {
+
+  @RequestMapping("/lobby")
+  public ModelAndView irALobby() {
+    Map<String, Object> model = new ModelMap();
+    return new ModelAndView("lobby", model);
+  }
+}
