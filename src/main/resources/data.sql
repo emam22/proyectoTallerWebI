44213f4092376fc09ea52fc692ec82c3ec848afa
emam22 <emamendez6@gmail.com>
2026-09-23 14:48:33 -0300

    fix: corregir nombre de tabla Usuario en data.sql (case-sensitive en Linux)

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
 INSERT INTO Usuario(id, email, password, rol, activo) VALUES(null, 'test@unlam.edu.ar', 'test', 'ADMIN', true);
 
-insert into usuario (activo,id,email,password,rol) Values(true,2,'admin@gmail.com', '1234', 'ADMIN');
+INSERT INTO Usuario(id, activo, email, password, rol) VALUES(2, true, 'admin@gmail.com', '1234', 'ADMIN');
 
-insert into usuario (activo,id,email,password,rol) Values(true,3,'usuario@gmail.com', '1234', 'USUARIO');
+INSERT INTO Usuario(id, activo, email, password, rol) VALUES(3, true, 'usuario@gmail.com', '1234', 'USUARIO');
 
